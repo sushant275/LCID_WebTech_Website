69e55c1a6be52884d9bb9da03a222dc6d83f7579
sushant275 <sushantshrestha774@gmail.com>
2026-02-28 12:36:49 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -99,7 +99,5 @@ def seed_database():
 
 if __name__ == '__main__':
     app = create_app()
-   app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-```
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
----
